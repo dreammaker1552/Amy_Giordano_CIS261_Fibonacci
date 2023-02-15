a5c0021cb2a56b2a53dc9c9254905a71b240bb2c
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-15 09:10:41 -0800

    Amy Giordano

--- Amy_Giordano_CIS261_Fibonacci.py
+++ Amy_Giordano_CIS261_Fibonacci.py
@@ -1,1 +1,17 @@
+def fibonacci(n):
+    # Initialize the first two numbers in the sequence
+    fib_series = [0, 1]
+
+    # Generate the sequence up to the nth number
+    for i in range(2, n + 1):
+        # Calculate the next number in the sequence as the sum of the two preceding ones
+        next_num = fib_series[i-1] + fib_series[i-2]
+        # Add the next number to the sequence
+        fib_series.append(next_num)
+
+    # Return the generated sequence
+    return fib_series
+
+fib_series = fibonacci(16)
+print(fib_series)
 
